3ef6e17a6b83f2b37b1de0fd469c29a87f747da9
satriaprayoga <satria.prayoga@gmail.com>
2022-06-18 01:41:54 +0700

    fourth commit

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SatkerController;
+use App\Http\Controllers\SubUnitController;
 use App\Http\Controllers\UnitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,3 +23,4 @@ use Illuminate\Support\Facades\Route;
  */
 Route::resource('units',UnitController::class);
 Route::resource('satker',SatkerController::class);
+Route::resource('subunits',SubUnitController::class);
