c494d25243b8a21e91db2c6d66d5dd0d7bc6401a
satriaprayoga <satria.prayoga@gmail.com>
2022-06-24 00:02:03 +0700

    tenth commit

--- resources/js/pages/organization/ViewOrganization.js
+++ resources/js/pages/organization/ViewOrganization.js
@@ -1,3 +1,5 @@
+import { ClearOutlined, EditOutlined } from '@mui/icons-material';
+import { Button, ButtonGroup, Grid } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect } from 'react'
 import { useState } from 'react';
@@ -5,34 +7,55 @@ import { useParams } from 'react-router-dom';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 
-import './organization.scss';
+import './viewOrganization.scss';
 
 const ViewOrganization = () => {
-  const[unit,setUnit]=useState({});
-  let {kode}=useParams();
+  const [unit, setUnit] = useState({});
+  let { kode } = useParams();
 
-  const loadUnit= async () =>{
+  const loadUnit = async () => {
     try {
-      const response=await axios.get('/api/units/'+kode);
+      const response = await axios.get('/api/units/' + kode);
       console.log(response.data.unit)
       setUnit(response.data.unit)
     } catch (error) {
-      
+
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     loadUnit();
-  },[])
+  }, [])
 
 
   return (
     <div className="organization">
-        <Sidebar />
-        <div className="organizationContainer">
-            <Navbar />
-            <div className="listTitle">{unit.nama}</div>
+      <Sidebar />
+      <div className="organizationContainer">
+        <Navbar />
+        
+        <div className="listContainer">
+          <Grid container spacing={2}>
+            <Grid item xs={4} md={8}>
+              <div className="listTitle">
+                {unit.nama}
+              </div>
+              <p>{unit.singkatan} - {unit.kode}</p>
+            </Grid>
+            <Grid item xs={8} md={4}>
+              <div className='buttonGroup'>
+                <ButtonGroup variant='outlined'>
+                  <Button><EditOutlined /></Button>
+                  <Button><ClearOutlined /></Button>
+                </ButtonGroup>
+              </div>
+            </Grid>
+          </Grid>
         </div>
+        <div className="listContainer">
+          
+        </div>
+      </div>
     </div>
   )
 }
